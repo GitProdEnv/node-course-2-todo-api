d325168a67d6ef1cd1bb08400448ed66411788f2
GitProdEnv <kopfhoererimtest@web.de>
2018-05-03 19:50:11 +0200

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,19 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var connectPath;
+
+const dbUser = 'mLabDBQLU$ER';
+const dbPassword = 'not3db4ddk!';
+
+if (process.env.PORT) {
+    connectPath = `mongodb://${dbUser}:${dbPassword}@ds111410.mlab.com:11410/node-to-api`;
+} else {
+    connectPath = 'mongodb://localhost:27017/TodoApp';
+}
+
+
+mongoose.connect(connectPath);
 
 module.exports = {mongoose};
