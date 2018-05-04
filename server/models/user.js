4bcc7175de9d0c3d45e1795a863654c97b752abe
GitProdEnv <kopfhoererimtest@web.de>
2018-05-04 17:02:42 +0200

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,55 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         trim: true,
-        minlength: 1
-    }
+        minlength: 1,
+        unique: true,
+        validate: { // see mongoosejs.com/docs/validation.html
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    var userObject = user.toObject(); // user.toObject() is responsible for taking your mongoose variable user and converting it into a regular object where only the properties available on the document exists.
+
+    return _.pick(userObject, ['_id', 'email']);
+};
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens = user.tokens.concat([{access, token}]); // There were some problems
+    // user.tokens.push({access, token});
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+const User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
