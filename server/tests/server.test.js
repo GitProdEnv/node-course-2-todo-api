8731ea4cec6edf614606a57dfdbe2dd65ae552fc
GitProdEnv <kopfhoererimtest@web.de>
2018-05-05 10:28:42 +0200

    Add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -223,7 +223,7 @@ describe('POST /users', () => {
                     expect(user).toExist();
                     expect(user.password).toNotBe(password);
                     done();
-                });
+                }).catch((e) => done(e)); // If one of these assertions fails, it will look for a catch case it never finds one => done() get never gets called and test timeout no usefuseful error message
             });
     });
 
@@ -249,5 +249,57 @@ describe('POST /users', () => {
             .expect(400)
             .end(done)
         ;
+    });
+});
+
+describe('POST /users/login', () => {
+
+    it('should login user and return auth token', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.header['x-auth']).toExist();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens[0]).toInclude({
+                        access: 'auth',
+                        token: res.headers['x-auth']
+                    });
+                    done();
+                }).catch((e) => done(e)); // If one of these assertions fails, it will look for a catch case it never finds one => done() get never gets called and test timeout no usefuseful error message
+            })
+    });
+
+    it('should reject invalid login', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password + '1'
+            })
+            .expect(400)
+            .expect((res) => {
+                expect(res.header['x-auth']).toNotExist();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done('Special Error Handling when x-auth toExist() is being called' + err);
+                }
+
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e)); 
+            })
     });
 });
